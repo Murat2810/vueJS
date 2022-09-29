b8c5f4c2ad10e190c6a9eef81d65326994d64022
Murat Menevse <bootcamp@adana01.de>
2022-09-29 10:51:07 +0200

    uebungen

--- SoloUebungen/script.js
+++ SoloUebungen/script.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
       userName: "John Doe",
       vTextExampleText:
         "Dieser Text ist mit vue.js kompiliert! Attribut v-text",
+      width: 400,
+      height: 400,
+      
+      // v-bind per element method
+      imgAttrs: {
+        class: "red",
+        src: "https://picsum.photos/200/300",
+        alt: "Hard to describe",
+        title: "Something random from picsum",
+      },
     };
   },
   directives: {
