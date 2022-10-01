269b1a71527199ea19a0f102b4cab183559985eb
Murat Menevse <bootcamp@adana01.de>
2022-10-01 16:13:50 +0200

    v-for

--- SoloUebungen/script.js
+++ SoloUebungen/script.js
@@ -6,7 +6,7 @@ const app = Vue.createApp({
         "Dieser Text ist mit vue.js kompiliert! Attribut v-text",
       width: 400,
       height: 400,
-      
+
       // v-bind per element method
       imgAttrs: {
         class: "red",
@@ -14,6 +14,57 @@ const app = Vue.createApp({
         alt: "Hard to describe",
         title: "Something random from picsum",
       },
+
+      // v-for mit Basic Array
+      fruits: [
+        "Apple",
+        "Banana",
+        "Melon",
+        "Blueberry",
+        "Strawberry",
+        "Ananas",
+        "Mango",
+      ],
+
+
+      // v-for mit object literal
+      fruitsObjects: {
+        Apple: "🍎",
+        Banana: "🍌",
+        Melon: "🍉",
+        Strawberry: "🍓",
+        Ananas: "🍍",
+      },
+
+      // v-for mit json ähnlichen Daten
+      fruitsJson: [
+         	{
+        id: 41,
+        name: "Apple",
+        emoji: "🍎",
+     	},
+        {
+        id: 22,
+        name: "Melon",
+        emoji: "🍉",
+	},
+        {
+        id: 32,
+        name: "Banana",
+        emoji: "🍌",
+	},
+	{
+	id: 92,
+        name: "Strawberry",
+        emoji: "🍓",
+	},
+	{      
+	id: 52,
+        name: "Ananas",
+        emoji: "🍍",
+	},
+      ],
+
     };
   },
   directives: {
